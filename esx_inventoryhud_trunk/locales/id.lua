1bfa00bcc0c7dea9c932bbf3a089b5d5ae5b03f7
Jeree <76772039+JeremyGregory@users.noreply.github.com>
2021-02-01 02:47:06 +0700

    Add files via upload

---
+++ esx_inventoryhud_trunk/locales/id.lua
@@ -0,0 +1,14 @@
+Locales["id"] = {
+  ["trunk_closed"] = "Bagasi Kendaraan Terkunci",
+  ["no_veh_nearby"] = "Tidak Ada Kendaraan Sekitar",
+  ["trunk_in_use"] = "Bagasi Sedang Dibuka",
+  ["trunk_full"] = "Bagasi Sudah Penuh",
+  ["invalid_quantity"] = "Jumlah Invalid",
+  ["cant_carry_more"] = "Kamu Tidak Bisa Menambahkan Barang Lagi",
+  ["nacho_veh"] = "Ini Bukan Kendaraan Kamu",
+  ["invalid_amount"] = "Jumlah Invalid",
+
+  ["insufficient_space"] = "Inventaris Tidak Cukup",
+  ["trunk_info"] = "<h3>Bagasi Kendaraan</h3><br><strong>Plat:</strong> %s<br><strong>Muatan:</strong> %s / %s KG",
+  ["player_inv_no_space"] = "Kamu Tidak Bisa Menambahkan Barang Lagi"
+}
